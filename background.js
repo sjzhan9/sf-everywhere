16b82eb63aca1e0c592da5d702423f27f57610e3
S. J. Zhang <hi.sj.zhang@gmail.com>
2022-12-10 18:23:42 -0500

    updated to v3

--- background.js
+++ background.js
@@ -2,125 +2,122 @@ let isOn = false;
 
 //make updates
 //update all tabs
-chrome.tabs.query({"currentWindow": true}, function (tabs) {
-    for (tab of tabs) {
-        if (tab.url) {
-            let id = tab.id;
-            updateTab(id);
-        }
+chrome.tabs.query({ currentWindow: true }, function (tabs) {
+  for (tab of tabs) {
+    if (tab.url) {
+      let id = tab.id;
+      updateTab(id);
     }
-})
-
+  }
+});
 
 //when new tab activated, check isOn to toggle the change
 chrome.tabs.onActivated.addListener(function (info) {
-    chrome.tabs.query({"active": true}, function (tabs) {
-        let id = tabs[0].id;
-        updateTab(id);
-    })
-})
+  chrome.tabs.query({ active: true }, function (tabs) {
+    let id = tabs[0].id;
+    updateTab(id);
+  });
+});
 
 //on new tab visit and refreshed check state
 chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
-    console.log("tab " + tabId + " updated");
-    if (tab.url) {
-        if (info.status === 'complete') {
-            updateTab(tabId);
-            }
+  console.log("tab " + tabId + " updated");
+  if (tab.url) {
+    if (info.status === "complete") {
+      updateTab(tabId);
     }
-})
+  }
+});
 
-function updateTab(id){
-    if (!isOn) {
-        turnOff();
-        chrome.tabs.sendMessage(id, 'turnOff');
-    } else {
-        turnOn();
-        chrome.tabs.sendMessage(id, 'turnOn');
-    }
+function updateTab(id) {
+  if (!isOn) {
+    turnOff();
+    chrome.tabs.sendMessage(id, {
+      message: "turnOff",
+    });
+  } else {
+    turnOn();
+    chrome.tabs.sendMessage(id, {
+      message: "turnOn",
+    });
+  }
 }
 
 //when clicked, send message to toggle the current page and update isOn
-chrome.browserAction.onClicked.addListener(function (tab) {
-    let id = tab.id;
-    if (!isOn) {
-        turnOn();
-        chrome.tabs.sendMessage(id, 'turnOn');
-        console.log("sent message to turn on")
+chrome.action.onClicked.addListener(function (tab) {
+  let id = tab.id;
+  if (!isOn) {
+    turnOn();
+    chrome.tabs.sendMessage(id, {
+      message: "turnOn",
+    });
+    console.log("sent message to turn on");
+  } else {
+    turnOff();
+    chrome.tabs.sendMessage(id, {
+      message: "turnOff",
+    });
+    console.log("sent message to turn off");
+  }
+});
 
-    } else {
-        turnOff();
-        chrome.tabs.sendMessage(id, 'turnOff');
-        console.log("sent message to turn off")
-
-    }
-})
-
-function turnOn(){
-    isOn = true;
-    chrome.browserAction.setTitle({title: 'SF ON'}, function(){
-        updateOnIcon();
-    })
+function turnOn() {
+  isOn = true;
+  chrome.action.setTitle({ title: "SF ON" }, function () {
+    updateOnIcon();
+  });
 }
 
-function turnOff(){
-    isOn = false;
-    chrome.browserAction.setTitle({title: 'SF OFF'}, function(){
-        updateOffIcon();
-    })
-
+function turnOff() {
+  isOn = false;
+  chrome.action.setTitle({ title: "SF OFF" }, function () {
+    updateOffIcon();
+  });
 }
 
-function updateOnIcon(){
-    if(isDarkModeEnabled() == false){
-        chrome.browserAction.setIcon({
-            path: "on-icon.png"
-          });
-    }else{
-        chrome.browserAction.setIcon({
-            path: "on-icon-dark.png"
-          });
-    }
+function updateOnIcon() {
+  if (isDarkModeEnabled() == false) {
+    chrome.action.setIcon({
+      path: "on-icon.png",
+    });
+  } else {
+    chrome.action.setIcon({
+      path: "on-icon-dark.png",
+    });
+  }
 }
 
-function updateOffIcon(){
-    if(isDarkModeEnabled() == false){
-        chrome.browserAction.setIcon({
-            path: "off-icon.png"
-          });
-    }else{
-        chrome.browserAction.setIcon({
-            path: "off-icon-dark.png"
-          });
-    }
+function updateOffIcon() {
+  if (isDarkModeEnabled() == false) {
+    chrome.action.setIcon({
+      path: "off-icon.png",
+    });
+  } else {
+    chrome.action.setIcon({
+      path: "off-icon-dark.png",
+    });
+  }
 }
 
-//check darkmode update everytime switch tab and 500ms 
+//check darkmode update everytime switch tab and 500ms
 function isDarkModeEnabled() {
-    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+  return (
+    chrome.windows.getCurrent.matchMedia &&
+    chrome.windows.getCurrent.matchMedia("(prefers-color-scheme: dark)").matches
+  );
 }
 
 let lastDarkModeStatus = isDarkModeEnabled();
 
-setInterval(function(){
-    // chrome.tabs.query({"currentWindow": true}, function (tabs) {
-    //     for (tab of tabs) {
-    //         if (tab.url) {
-    //             let id = tab.id;
-    //             updateTab(id);
-    //             console.log("update fired")
-    //         }
-    //     }
-    // })
-
-    if (lastDarkModeStatus !== isDarkModeEnabled()){
-        //make update
-        if (!isOn) {
-            updateOffIcon()
-        } else {
-            updateOnIcon()
-        }
-        //update state
-        lastDarkModeStatus = isDarkModeEnabled();
-    };
-    }, 500);
+setInterval(function () {
+  if (lastDarkModeStatus !== isDarkModeEnabled()) {
+    //make update
+    if (!isOn) {
+      updateOffIcon();
+    } else {
+      updateOnIcon();
+    }
+    //update state
+    lastDarkModeStatus = isDarkModeEnabled();
+  }
+}, 500);
--- content.js
+++ content.js
@@ -1,8 +1,9 @@
 //inject class to all pages
-var style = document.createElement('style');
-style.type = 'text/css';
+var style = document.createElement("style");
+style.type = "text/css";
 
-var classes = "div,h1,h2,h3,h4,h5,h6,p,a,time,code,li,ul,ol,strong,span,button,strong,b,i,em,small,marked,sub,sup,body";
+var classes =
+  "div,h1,h2,h3,h4,h5,h6,p,a,time,code,li,ul,ol,strong,span,button,strong,b,i,em,small,marked,sub,sup,body";
 
 style.textContent = `
 .sf-everywhere {
@@ -11,29 +12,38 @@ letter-spacing: normal !important;
 }
 `;
 
-setTimeout(function(){ 
-    // Add the style tag to the <head> tag of the document
-document.head.appendChild(style);
- }, 10);
+setTimeout(function () {
+  // Add the style tag to the <head> tag of the document
+  document.head.appendChild(style);
+}, 10);
 
-chrome.runtime.onMessage.addListener(function(msg){
-    if (msg == "turnOff") {
-        turnOff();
-      } else {
-        turnOn();
-      }
-})
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message == "turnOff") {
+    turnOff();
+  } else {
+    turnOn();
+  }
+});
 
-function turnOff(){
-    let allElem = document.querySelectorAll(classes);
-    for (elem of allElem){
+function turnOff() {
+  let allElem = document.querySelectorAll(classes);
+  for (elem of allElem) {
     elem.classList.remove("sf-everywhere");
-    }
+    X;
+  }
 }
 
-function turnOn(){
-    let allElem = document.querySelectorAll(classes);
-    for (elem of allElem){
+function turnOn() {
+  let allElem = document.querySelectorAll(classes);
+  for (elem of allElem) {
     elem.classList.add("sf-everywhere");
-    }
+  }
 }
+
+// const init = function () {
+//   const injectElement = document.createElement("div");
+//   injectElement.className = "rustyZone-element";
+//   injectElement.innerHTML = "Hello From the Rusty Zone Element";
+//   document.body.appendChild(injectElement);
+// };
+// init();
